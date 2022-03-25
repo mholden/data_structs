274749705f52b40aa8bb9553c994059017928873
Max Holden <maxholden11@gmail.com>
2022-03-25 15:55:37 -0400

    more test cases for ud graph shortest paths

--- graphs/test_ud_graph.c
+++ graphs/test_ud_graph.c
@@ -119,6 +119,7 @@ static void test_specific_case1(void) {
     
     path_iter = 0;
     assert(ll_iterate(spath, test_specific_case1_spath_cb, (void *)&path_iter) == 0);
+    assert(path_iter == 3);
     
     //udg_dump(g);
     ll_destroy(spath);
@@ -201,7 +202,7 @@ static void test_random_case(int n) {
 
 static int test_iterate_cb(void *data, void *ctx, bool *stop) {
     test_udg_data_t *tud = (test_udg_data_t *)data;
-    printf("    test_iterate_cb %d\n", tud->tud_key);
+    //printf("    test_iterate_cb %d\n", tud->tud_key);
     return 0;
 }
 
@@ -221,23 +222,363 @@ static void test_iterate(void) {
     //udg_dump(g);
     
     assert(udg_get_node(g, (void *)&tud[rand() % n], &start_node) == 0);
-    printf("  doing udg_iterate_df with start %d\n", ((test_udg_data_t *)start_node->n_data)->tud_key);
+    //printf("  doing udg_iterate_df with start %d\n", ((test_udg_data_t *)start_node->n_data)->tud_key);
     assert(udg_iterate_df(g, start_node, test_iterate_cb, NULL) == 0);
-    printf("  doing udg_iterate_bf with start %d\n", ((test_udg_data_t *)start_node->n_data)->tud_key);
+    //printf("  doing udg_iterate_bf with start %d\n", ((test_udg_data_t *)start_node->n_data)->tud_key);
     assert(udg_iterate_bf(g, start_node, test_iterate_cb, NULL) == 0);
     
     udg_destroy(g);
     free(tud);
 }
 
-static void test_shortest_path(void) {
+static int test_shortest_path_specific_case4_cb(void *data, void *ctx, bool *stop) {
+    udg_node_t *n = (udg_node_t *)data;
+    test_udg_data_t *tud = (test_udg_data_t *)n->n_data;
+    int *path_iter = (int *)ctx;
+    int err;
+    
+    switch (*path_iter) {
+        case 0:
+            assert(tud->tud_key == 0);
+            break;
+        case 1:
+            assert(tud->tud_key == 7);
+            break;
+        case 2:
+            assert(tud->tud_key == 3);
+            break;
+        default:
+            assert(0);
+            break;
+    }
+    
+    (*path_iter)++;
+    
+    return 0;
+    
+error_out:
+    return err;
+}
+
+//
+// graph definition:
+//   0: 1, 4, 7
+//   1: 0, 2
+//   2: 1, 3
+//   3: 2
+//   4: 0, 5
+//   5: 4, 6
+//   6: 5
+//   7: 0, 3, 8
+//   8: 7, 9
+//   9: 8, 10
+//   10: 9
+//
+// same as case 3 but connect the 7 and 3 and make
+// sure we still report 0-7-3 as the shortest path.
+// shouldn't look at the 8 in this case after finding
+// 3 on the 7 path, either
+//
+static void test_shortest_path_specific_case4(void) {
+    ud_graph_t *g;
+    test_udg_data_t tud[11];
+    udg_node_t *n[11];
+    linked_list_t *spath;
+    int path_iter;
+    
+    printf("test_shortest_path_specific_case4\n");
+    
+    assert(g = udg_create(&test_udg_ops));
+    memset(tud, 0, sizeof(test_udg_data_t) * 11);
+    
+    for (int i = 0; i < 11; i++) {
+        tud[i].tud_key = i;
+        assert(udg_add_node(g, (void *)&tud[i], &n[i]) == 0);
+    }
+    udg_check(g);
+    
+    assert(udg_add_edge(g, n[0], n[1]) == 0);
+    assert(udg_add_edge(g, n[0], n[4]) == 0);
+    assert(udg_add_edge(g, n[0], n[7]) == 0);
+    assert(udg_add_edge(g, n[1], n[2]) == 0);
+    assert(udg_add_edge(g, n[2], n[3]) == 0);
+    assert(udg_add_edge(g, n[4], n[5]) == 0);
+    assert(udg_add_edge(g, n[5], n[6]) == 0);
+    assert(udg_add_edge(g, n[7], n[3]) == 0);
+    assert(udg_add_edge(g, n[7], n[8]) == 0);
+    assert(udg_add_edge(g, n[8], n[9]) == 0);
+    assert(udg_add_edge(g, n[9], n[10]) == 0);
+    udg_check(g);
+    
+    assert(udg_shortest_path_df(g, n[0], n[3], &spath) == 0);
+    assert(spath);
+    
+    path_iter = 0;
+    assert(ll_iterate(spath, test_shortest_path_specific_case4_cb, (void *)&path_iter) == 0);
+    assert(path_iter == 3);
+    
+    //udg_dump(g);
+    ll_destroy(spath);
+    udg_destroy(g);
+}
+
+static int test_shortest_path_specific_case3_cb(void *data, void *ctx, bool *stop) {
+    udg_node_t *n = (udg_node_t *)data;
+    test_udg_data_t *tud = (test_udg_data_t *)n->n_data;
+    int *path_iter = (int *)ctx;
+    int err;
+    
+    switch (*path_iter) {
+        case 0:
+            assert(tud->tud_key == 0);
+            break;
+        case 1:
+            assert(tud->tud_key == 1);
+            break;
+        case 2:
+            assert(tud->tud_key == 2);
+            break;
+        case 3:
+            assert(tud->tud_key == 3);
+            break;
+        default:
+            assert(0);
+            break;
+    }
+    
+    (*path_iter)++;
+    
+    return 0;
+    
+error_out:
+    return err;
+}
+
+//
+// graph definition:
+//   0: 1, 4, 7
+//   1: 0, 2
+//   2: 1, 3
+//   3: 2
+//   4: 0, 5
+//   5: 4, 6
+//   6: 5
+//   7: 0, 8
+//   8: 7, 9
+//   9: 8, 10
+//   10: 9
+//
+// get shortest path from 0 to 3. when we find the 3
+// on the 1 path (0-1-2-3), this should cause us not
+// to look at the 6, 9, or 10
+//
+static void test_shortest_path_specific_case3(void) {
+    ud_graph_t *g;
+    test_udg_data_t tud[11];
+    udg_node_t *n[11];
+    linked_list_t *spath;
+    int path_iter;
+    
+    printf("test_shortest_path_specific_case3\n");
+    
+    assert(g = udg_create(&test_udg_ops));
+    memset(tud, 0, sizeof(test_udg_data_t) * 11);
+    
+    for (int i = 0; i < 11; i++) {
+        tud[i].tud_key = i;
+        assert(udg_add_node(g, (void *)&tud[i], &n[i]) == 0);
+    }
+    udg_check(g);
+    
+    assert(udg_add_edge(g, n[0], n[1]) == 0);
+    assert(udg_add_edge(g, n[0], n[4]) == 0);
+    assert(udg_add_edge(g, n[0], n[7]) == 0);
+    assert(udg_add_edge(g, n[1], n[2]) == 0);
+    assert(udg_add_edge(g, n[2], n[3]) == 0);
+    assert(udg_add_edge(g, n[4], n[5]) == 0);
+    assert(udg_add_edge(g, n[5], n[6]) == 0);
+    assert(udg_add_edge(g, n[7], n[8]) == 0);
+    assert(udg_add_edge(g, n[8], n[9]) == 0);
+    assert(udg_add_edge(g, n[9], n[10]) == 0);
+    udg_check(g);
+    
+    assert(udg_shortest_path_df(g, n[0], n[3], &spath) == 0);
+    assert(spath);
+    
+    path_iter = 0;
+    assert(ll_iterate(spath, test_shortest_path_specific_case3_cb, (void *)&path_iter) == 0);
+    assert(path_iter == 4);
+    
+    //udg_dump(g);
+    ll_destroy(spath);
+    udg_destroy(g);
+}
+
+static int test_shortest_path_specific_case2_cb(void *data, void *ctx, bool *stop) {
+    udg_node_t *n = (udg_node_t *)data;
+    test_udg_data_t *tud = (test_udg_data_t *)n->n_data;
+    int *path_iter = (int *)ctx;
+    int err;
+    
+    switch (*path_iter) {
+        case 0:
+            assert(tud->tud_key == 0);
+            break;
+        case 1:
+            assert(tud->tud_key == 1);
+            break;
+        case 2:
+            assert(tud->tud_key == 6);
+            break;
+        default:
+            assert(0);
+            break;
+    }
+    
+    (*path_iter)++;
+    
+    return 0;
+    
+error_out:
+    return err;
+}
+
+//
+// graph definition:
+//   0: 1, 2, 4
+//   1: 0, 6
+//   2: 0, 3
+//   3: 2
+//   4: 0, 5
+//   5: 4
+//   6: 1
+//
+// get shortest path from 0 to 6. when we find the 6
+// on the 1 path (0-1-6), we should then see that we
+// have the path when iterating 2 and 4, and should
+// not even look at the 3 and 5
+//
+static void test_shortest_path_specific_case2(void) {
+    ud_graph_t *g;
+    test_udg_data_t tud[7];
+    udg_node_t *n[7];
+    linked_list_t *spath;
+    int path_iter;
+    
+    printf("test_shortest_path_specific_case2\n");
+    
+    assert(g = udg_create(&test_udg_ops));
+    memset(tud, 0, sizeof(test_udg_data_t) * 7);
+    
+    for (int i = 0; i < 7; i++) {
+        tud[i].tud_key = i;
+        assert(udg_add_node(g, (void *)&tud[i], &n[i]) == 0);
+    }
+    udg_check(g);
+    
+    assert(udg_add_edge(g, n[0], n[1]) == 0);
+    assert(udg_add_edge(g, n[0], n[2]) == 0);
+    assert(udg_add_edge(g, n[0], n[4]) == 0);
+    assert(udg_add_edge(g, n[1], n[6]) == 0);
+    assert(udg_add_edge(g, n[2], n[3]) == 0);
+    assert(udg_add_edge(g, n[4], n[5]) == 0);
+    udg_check(g);
+    
+    assert(udg_shortest_path_df(g, n[0], n[6], &spath) == 0);
+    assert(spath);
+    
+    path_iter = 0;
+    assert(ll_iterate(spath, test_shortest_path_specific_case2_cb, (void *)&path_iter) == 0);
+    assert(path_iter == 3);
+    
+    //udg_dump(g);
+    ll_destroy(spath);
+    udg_destroy(g);
+}
+
+static int test_shortest_path_specific_case1_cb(void *data, void *ctx, bool *stop) {
+    udg_node_t *n = (udg_node_t *)data;
+    test_udg_data_t *tud = (test_udg_data_t *)n->n_data;
+    int *path_iter = (int *)ctx;
+    int err;
+    
+    switch (*path_iter) {
+        case 0:
+            assert(tud->tud_key == 0);
+            break;
+        case 1:
+            assert(tud->tud_key == 4);
+            break;
+        default:
+            assert(0);
+            break;
+    }
+    
+    (*path_iter)++;
+    
+    return 0;
+    
+error_out:
+    return err;
+}
+
+//
+// graph definition:
+//   0: 1, 2, 3, 4, 5, 6, 7
+//   1: 0
+//   2: 0
+//   3: 0
+//   4: 0
+//   5: 0
+//   6: 0
+//   7: 0
+//
+// get shortest path from 0 to 4. we should stop iterating
+// adjacencies as soon as we find 4
+//
+static void test_shortest_path_specific_case1(void) {
+    ud_graph_t *g;
+    test_udg_data_t tud[8];
+    udg_node_t *n[8];
+    linked_list_t *spath;
+    int path_iter;
+    
+    printf("test_shortest_path_specific_case1\n");
+    
+    assert(g = udg_create(&test_udg_ops));
+    memset(tud, 0, sizeof(test_udg_data_t) * 8);
+    
+    for (int i = 0; i < 8; i++) {
+        tud[i].tud_key = i;
+        assert(udg_add_node(g, (void *)&tud[i], &n[i]) == 0);
+    }
+    udg_check(g);
+    
+    for (int i = 1; i < 8; i++) {
+        assert(udg_add_edge(g, n[0], n[i]) == 0);
+    }
+    udg_check(g);
+    
+    assert(udg_shortest_path_df(g, n[0], n[4], &spath) == 0);
+    assert(spath);
+    
+    path_iter = 0;
+    assert(ll_iterate(spath, test_shortest_path_specific_case1_cb, (void *)&path_iter) == 0);
+    assert(path_iter == 2);
+    
+    //udg_dump(g);
+    ll_destroy(spath);
+    udg_destroy(g);
+}
+
+static void test_shortest_path_random(void) {
     ud_graph_t *g;
     test_udg_data_t *tud, *_tud1, *_tud2;
     udg_node_t *from, *to;
     linked_list_t *spath;
     int n = 128;
     
-    printf("test_shortest_path\n");
+    printf("test_shortest_path_random\n");
     
     assert(g = udg_create(&test_udg_ops));
     assert(tud = malloc(sizeof(test_udg_data_t) * n));
@@ -251,19 +592,27 @@ static void test_shortest_path(void) {
     assert(udg_get_node(g, (void *)_tud1, &from) == 0);
     assert(udg_get_node(g, (void *)_tud2, &to) == 0);
     
-    printf("  getting shortest path from %d to %d...\n", _tud1->tud_key, _tud2->tud_key);
+    //printf("  getting shortest path from %d to %d...\n", _tud1->tud_key, _tud2->tud_key);
     assert(udg_shortest_path_df(g, from, to, &spath) == 0);
     
     if (spath) {
-        printf("    spath is: ");
-        ll_dump(spath);
+        //printf("    spath is: ");
+        //ll_dump(spath);
         ll_destroy(spath);
     } else {
-        printf("    no path\n");
+       //printf("    no path\n");
     }
     
     udg_destroy(g);
     free(tud);
+}
+
+static void test_shortest_path(void) {
+    test_shortest_path_random();
+    test_shortest_path_specific_case1();
+    test_shortest_path_specific_case2();
+    test_shortest_path_specific_case3();
+    test_shortest_path_specific_case4();
 }
 
 #define DEFAULT_NUM_ELEMENTS (1 << 10)
